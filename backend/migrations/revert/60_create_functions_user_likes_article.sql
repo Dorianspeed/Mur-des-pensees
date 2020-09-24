b772130abb9e18f2e76c56c85d123369767486bf
Dorianspeed <Dorianspeed@hotmail.com>
2020-09-24 15:32:27 +0200

    fix: handle revoke execute on functions from user_likes_article

--- backend/migrations/revert/60_create_functions_user_likes_article.sql
+++ backend/migrations/revert/60_create_functions_user_likes_article.sql
@@ -2,21 +2,21 @@
 
 BEGIN;
 
-GRANT EXECUTE ON FUNCTION "insert_like"(INT, INT) TO "reader";
-GRANT EXECUTE ON FUNCTION "insert_like"(INT, INT) TO "editor";
-GRANT EXECUTE ON FUNCTION "insert_like"(INT, INT) TO "chief_editor";
+REVOKE EXECUTE ON FUNCTION "insert_like"(INT, INT) FROM "reader";
+REVOKE EXECUTE ON FUNCTION "insert_like"(INT, INT) FROM "editor";
+REVOKE EXECUTE ON FUNCTION "insert_like"(INT, INT) FROM "chief_editor";
 
-GRANT EXECUTE ON FUNCTION "delete_like"(INT, INT) TO "reader";
-GRANT EXECUTE ON FUNCTION "delete_like"(INT, INT) TO "editor";
-GRANT EXECUTE ON FUNCTION "delete_like"(INT, INT) TO "chief_editor";
+REVOKE EXECUTE ON FUNCTION "delete_like"(INT, INT) FROM "reader";
+REVOKE EXECUTE ON FUNCTION "delete_like"(INT, INT) FROM "editor";
+REVOKE EXECUTE ON FUNCTION "delete_like"(INT, INT) FROM "chief_editor";
 
-GRANT EXECUTE ON FUNCTION "check_like"(INT, INT) TO "reader";
-GRANT EXECUTE ON FUNCTION "check_like"(INT, INT) TO "editor";
-GRANT EXECUTE ON FUNCTION "check_like"(INT, INT) TO "chief_editor";
+REVOKE EXECUTE ON FUNCTION "check_like"(INT, INT) FROM "reader";
+REVOKE EXECUTE ON FUNCTION "check_like"(INT, INT) FROM "editor";
+REVOKE EXECUTE ON FUNCTION "check_like"(INT, INT) FROM "chief_editor";
 
 DROP FUNCTION "insert_like"(INT, INT);
 DROP FUNCTION "delete_like"(INT, INT);
 DROP FUNCTION "check_like"(INT, INT);
-DROP FUNCTIOn "get_likes_by_article"(INT);
+DROP FUNCTION "get_likes_by_article"(INT);
 
 COMMIT;
