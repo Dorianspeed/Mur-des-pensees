acb247497e6629f101cf533ffcec0261e2055239
Dorianspeed <Dorianspeed@hotmail.com>
2020-09-26 18:08:48 +0200

    feat: add minimum condition to password validation

--- backend/graphQL/validation/insertUser.js
+++ backend/graphQL/validation/insertUser.js
@@ -5,7 +5,7 @@ module.exports = Joi.object({
     firstname: Joi.string().pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u).required(),
     lastname: Joi.string().pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    password: Joi.string().min(8).required(),
     confirmed_password: Joi.ref('password'),
     avatar_url: Joi.string().uri({allowRelative: true, relativeOnly: true}).required()
 }).required();
