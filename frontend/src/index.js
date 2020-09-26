dd1b41061392ef0cc8359ac0a99a313d32ffe9ef
Dorianspeed <Dorianspeed@hotmail.com>
2020-09-26 15:26:00 +0200

    feat: handle state persistence with redux-persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 // == Import : local
 import App from './containers/App';
-import store from './store';
+import { store, persistor } from './store';
 
 // == Render
 const rootReactElement = (
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>
 );
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,19 @@
 // == Import : npm
 import { createStore, compose, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 // == Import : local
 import rootReducer from './reducers';
 import ajaxMiddleware from './middleware/ajaxMiddleware';
 import userMiddleware from './middleware/userMiddleware';
+
+// == Configuration du persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
 
 // == Enhancers
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -16,11 +25,15 @@ const enhancers = composeEnhancers(
   ),
 );
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // == Store
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   enhancers,
 );
 
+const persistor = persistStore(store);
+
 // == Export
-export default store;
+export { store, persistor };
