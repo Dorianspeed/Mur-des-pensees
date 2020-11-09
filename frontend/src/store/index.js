a2896a44b9cb1067b14b4512a06005e5001fb091
Dorian Garcia <Dorianspeed@hotmail.com>
2020-11-09 11:53:16 +0100

    refactor: remove persistent state for articles

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -14,7 +14,7 @@ import adminMiddleware from './middleware/adminMiddleware';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['user', 'articles'],
+  whitelist: ['user'],
 };
 
 // == Enhancers
